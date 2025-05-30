2ec407ecf9dd91bd2689ab3224a9791b4552ffc0
Yars2021 <yaroslav.sukhovey@yandex.ru>
2025-05-30 19:39:54 +0300

    + cors

--- src/main/java/ru/itmo/s312198/devops/market/CorsConfig.java
+++ src/main/java/ru/itmo/s312198/devops/market/CorsConfig.java
@@ -5,15 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/*
+
 @Configuration
 @EnableWebMvc
-*/
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost", "http://localhost:8080")
+                .allowedOrigins("http://localhost", "http://localhost:8080", "http://localhost:3000")
                 .allowedMethods("*");
     }
 }
